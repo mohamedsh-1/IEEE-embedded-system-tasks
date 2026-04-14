f9b70cac4f2bc3c2bbc03972f3aa4533d3b4aa4c
mohamedsh-1 <mohamed.sameh01282923517@gmail.com>
2026-04-14 03:43:47 +0200

    Create matrix.h with matrix operations structure and function declarations

---
+++ tasks/matrix.h
@@ -0,0 +1,18 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+typedef struct {
+    int rows;
+    int cols;
+    int** data;
+} Matrix;
+
+// Function declarations
+Matrix createMatrix(int r, int c);
+void inputMatrix(Matrix m);
+void printMatrix(Matrix m);
+Matrix addMatrix(Matrix a, Matrix b);
+Matrix subMatrix(Matrix a, Matrix b);
+void freeMatrix(Matrix m);
+
+#endif
