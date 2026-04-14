ce5ca49a378827fb063109a54dfdb5e1d39322ee
mohamedsh-1 <mohamed.sameh01282923517@gmail.com>
2026-04-14 04:05:14 +0200

    Define Matrix structure and operations in matric.h
    Added Matrix structure and function declarations for matrix operations.

--- tasks/task3/matric.h
+++ tasks/task3/matric.h
@@ -1,1 +1,17 @@
+#ifndef MATRIX_H
+#define MATRIX_H
 
+typedef struct {
+    int rows;
+    int cols;
+    int **data;
+} Matrix;
+
+Matrix createMatrix(int r, int c);
+void inputMatrix(Matrix m);
+void printMatrix(Matrix m);
+
+Matrix addMatrix(Matrix a, Matrix b);
+Matrix subMatrix(Matrix a, Matrix b);
+
+#endif
